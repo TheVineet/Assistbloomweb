6e101ac8eed8d1e35804d225f307c8c50d5298c5
Vineet Pandey <vineet08official@gmail.com>
2024-06-24 14:39:57 +0530

    new changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3017046170"
+    serviceWorkerVersion: "3109428258"
   }
 });
